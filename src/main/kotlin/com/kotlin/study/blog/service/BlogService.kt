e0ede1398b7420311b73824fe3d796407ea4b9b9
leekyuwon <kyuwoon369@gmail.com>
2023-09-28 22:32:21 +0900

    카카오 블로그 검색 api를 webFlux로 호출하라

--- src/main/kotlin/com/kotlin/study/blog/service/BlogService.kt
+++ src/main/kotlin/com/kotlin/study/blog/service/BlogService.kt
@@ -1,11 +1,39 @@
 package com.kotlin.study.blog.service
 
 import com.kotlin.study.blog.dto.BlogDto
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders.CONTENT_TYPE
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
 
 @Service
 class BlogService {
+    @Value("\${REST_API_KEY}")
+    lateinit var restApiKey: String
     fun searchKakao(blogDto: BlogDto): String? {
-        return "searchKakao";
+        val webClient = WebClient.builder()
+            .baseUrl("https://dapi.kakao.com")
+            .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .build()
+
+        val response = webClient
+            .get()
+            .uri {
+                it.path("/v2/search/blog")
+                    .queryParam("query", blogDto.query)
+                    .queryParam("sort", blogDto.sort)
+                    .queryParam("page", blogDto.page)
+                    .queryParam("size", blogDto.size)
+                    .build()
+            }
+            .header("Authorization", "KakaoAK $restApiKey")
+            .retrieve()
+            .bodyToMono<String>()
+
+        val result = response.block()
+
+        return result
     }
 }
