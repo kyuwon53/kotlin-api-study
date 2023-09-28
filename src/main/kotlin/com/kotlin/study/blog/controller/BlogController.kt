8415f36a356bb5c47b4f4bdec385da3f20b885ac
leekyuwon <kyuwoon369@gmail.com>
2023-09-28 22:08:47 +0900

    컨트롤러를 생성하라

---
+++ src/main/kotlin/com/kotlin/study/blog/controller/BlogController.kt
@@ -0,0 +1,20 @@
+package com.kotlin.study.blog.controller
+
+import com.kotlin.study.blog.dto.BlogDto
+import com.kotlin.study.blog.service.BlogService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/api/blog")
+@RestController
+class BlogController(
+    val blogService: BlogService
+) {
+    @GetMapping("")
+    fun search(@RequestBody blogDto: BlogDto): String? {
+        val result = blogService.searchKakao(blogDto)
+        return result
+    }
+}
