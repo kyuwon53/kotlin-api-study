82a7bdf889eccf0832d378f585dd118cb644ef95
leekyuwon <kyuwoon369@gmail.com>
2023-09-28 21:58:13 +0900

    엔티티 생성

---
+++ src/main/kotlin/com/kotlin/study/blog/entity/Entities.kt
@@ -0,0 +1,10 @@
+package com.kotlin.study.blog.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+
+@Entity
+class WordCount(
+    @Id val word: String,
+    val cnt: Int = 0
+)
